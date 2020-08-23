1b6bd1475b5e94ae40599a7e326d3348f6afd2bc
Nisha <nishavijai@gmail.com>
2020-08-23 15:56:44 +0200

    ADD SVG AND UPDATE LINK DETAILS

--- src/assets/Components/LinksData.js
+++ src/assets/Components/LinksData.js
@@ -2,6 +2,7 @@ import YouTubeImg from "../Images/youtube-squared.svg";
 import MediumImg from "../Images/medium-monogram.svg";
 import DevToImg from "../Images/devto-icon.svg";
 import EggHeadImg from "../Images/eggheadio-icon.svg";
+import FreeCodeCampImg from "../Images/free-code-camp.svg";
 
 const LinksData = [
   {
@@ -45,6 +46,107 @@ const LinksData = [
     interactiveLevel: "Beginner",
     interactiveImg: EggHeadImg,
     interactiveAlt: "EggHead Logo Image",
+  },
+  {
+    id: 5,
+    articleTitle: "Articles",
+    articleURL:
+      "https://www.freecodecamp.org/news/what-i-learned-from-doing-60-technical-interviews-in-30-days/",
+    articleName: "60-technical-interviews-in-30-days",
+    articleLanguage: "Interview Tips from 30-days",
+    articleLevel: "Interview",
+    articleImg: FreeCodeCampImg,
+    articleAlt: "FreeCodeCamp Website Logo Image",
+  },
+  {
+    id: 6,
+    articleTitle: "Articles",
+    articleURL:
+      "https://dev.to/5t3ph/3-popular-website-heroes-created-with-css-grid-layout-4fdl?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email",
+    articleName: "3-popular-website-heroes-created-with-css-grid-layout",
+    articleLanguage: "CSS",
+    articleLevel: "CSS Grid",
+    articleImg: DevToImg,
+    articleAlt: "DevTo Website Logo Image",
+  },
+  {
+    id: 7,
+    articleTitle: "Articles",
+    articleURL:
+      "https://dev.to/nghiemthu/8-projects-with-designs-you-can-do-to-become-front-end-developer-4pf8?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email",
+    articleName:
+      "8-projects-with-designs-you-can-do-to-become-front-end-developer",
+    articleLanguage: "Front End Developer Projects",
+    articleLevel: "Front End",
+    articleImg: DevToImg,
+    articleAlt: "DevTo Website Logo Image",
+  },
+  {
+    id: 8,
+    articleTitle: "Articles",
+    articleURL:
+      "https://dev.to/nerdjfpb/15-vanilla-javascript-project-ideas-beginner-to-expert-with-free-tutorial-3c7a?utm_source=digest_mailer&utm_medium=email&utm_campaign=digest_email",
+    articleName: "15-vanilla-javascript-project-ideas-beginner-to-expert",
+    articleLanguage: "Vanilla JavaScript Project Ideas",
+    articleLevel: "Beginner to Expert",
+    articleImg: DevToImg,
+    articleAlt: "DevTo Website Logo Image",
+  },
+  {
+    id: 9,
+    linkTitle: "Video Tutorials",
+    linkURL:
+      "https://www.youtube.com/watch?v=XMEX9_HvW2k&list=PL0yztVlNIsg4msHzk1eEI2n3nJJylNONi&index=1",
+    linkName: "Advanced CSS",
+    linkLanguage: "CSS",
+    linkLevel: "Advanced",
+    linkImg: YouTubeImg,
+    linkAlt: "YouTube Logo Image",
+  },
+  {
+    id: 10,
+    articleTitle: "Articles",
+    articleURL:
+      "https://medium.com/javascript-in-plain-english/24-quick-fire-javascript-interview-questions-a71f78d03f08",
+    articleName: "24-quick-fire-javascript-interview-questions",
+    articleLanguage: "Javascript",
+    articleLevel: "Interview",
+    articleImg: MediumImg,
+    articleAlt: "Medium Website Logo Image",
+  },
+  {
+    id: 11,
+    articleTitle: "Articles",
+    articleURL:
+      "https://levelup.gitconnected.com/8-javascript-concepts-to-learn-for-tech-interviews-57623edd2b65",
+    articleName: "8-javascript-concepts-to-learn-for-tech-interviews",
+    articleLanguage: "Javascript",
+    articleLevel: "Interview",
+    articleImg: MediumImg,
+    articleAlt: "Medium Website Logo Image",
+  },
+  {
+    id: 12,
+    articleTitle: "Articles",
+    articleURL:
+      "https://medium.com/javascript-in-plain-english/5-simple-tips-to-write-better-arrow-functions-a55f40f63d58",
+    articleName: "5-simple-tips-to-write-better-arrow-functions",
+    articleLanguage: "Javascript",
+    articleLevel: "Intermediate",
+    articleImg: MediumImg,
+    articleAlt: "Medium Website Logo Image",
+  },
+  {
+    id: 13,
+    articleTitle: "Articles",
+    articleURL:
+      "https://egghead.io/lessons/css-use-the-border-radius-css-property-to-define-horizontal-and-vertical-radius",
+    articleName:
+      "the-border-radius-css-property-horizontal-and-vertical-radius",
+    articleLanguage: "CSS",
+    articleLevel: "Intermediate",
+    articleImg: EggHeadImg,
+    articleAlt: "EggHead Website Logo Image",
   },
 ];
 
